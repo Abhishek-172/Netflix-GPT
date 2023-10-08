450e42ca0efd29fc407e71ad14c135343e2c4d86
Abhishek Pandey <abhishek.pandey172@gmail.com>
2023-10-08 20:59:50 +0530

    Added useRef Hook and did Form Validation of SignIn & SignUp

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,19 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Header from './Header';
+import { checkValidData } from '../utils/validate';
 
 const Login = () => {
     const [isSignInForm, setIsSignInForm] = useState(true);
+    const [errorMessage, setErrorMesage] = useState(null);
+    const email = useRef(null);
+    const password = useRef(null);
+    const name = useRef(null);
     const toggleSignInForm = () => {
         setIsSignInForm(!isSignInForm);
+    }
+    const handleButtonClick = () => {
+        const message = checkValidData(email.current.value, password.current.value);
+        setErrorMesage(message);    
     }
     return (
         <div>
@@ -13,12 +22,13 @@ const Login = () => {
                 <img src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"
                     alt="logo" />
             </div>
-            <form className='w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80'>
-                <h1 className='font-bold text-3xl py-4'>{ isSignInForm? "Sign In": "Sign Up"}</h1>
-                <input type='text' placeholder='Email Address' className='p-2 my-4 w-full bg-gray-700 rounded-lg' />
-                {!isSignInForm && <input type='text' placeholder='Full Name' className='p-2 my-4 w-full bg-gray-700 rounded-lg' />}
-                <input type='password' placeholder='Password' className='p-2 my-4 w-full bg-gray-700 rounded-lg' />
-                <button className='p-2 my-4 bg-red-700 w-full rounded-lg'>{ isSignInForm? "Sign In": "Sign Up"}</button>
+            <form onSubmit={(e)=> e.preventDefault()} className='w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80'>
+                <h1 className='font-bold text-3xl py-4'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
+                {!isSignInForm && <input ref={ name } type='text' placeholder='Full Name' className='p-2 my-4 w-full bg-gray-700 rounded-lg' />}
+                <input ref={ email } type='text' placeholder='Email Address' className='p-2 my-4 w-full bg-gray-700 rounded-lg' />
+                <input ref={ password } type='password' placeholder='Password' className='p-2 my-4 w-full bg-gray-700 rounded-lg' />
+                <p className='text-red-500 py-2'>{ errorMessage }</p>
+                <button className='p-2 my-4 bg-red-700 w-full rounded-lg' onClick={handleButtonClick}>{isSignInForm ? "Sign In" : "Sign Up"}</button>
                 <p className='py-4 cursor-pointer' onClick={toggleSignInForm}>{ isSignInForm? "New to Netflix? Sign Up Now": "Already registered? Sign In Now"}</p>
             </form>
         </div>
---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDpX9T93okpEj5d0yGynjZozl8srfsf4Ig",
+  authDomain: "netflixgpt-e6643.firebaseapp.com",
+  projectId: "netflixgpt-e6643",
+  storageBucket: "netflixgpt-e6643.appspot.com",
+  messagingSenderId: "133525821671",
+  appId: "1:133525821671:web:a0033b6e80a8e5efa56a42",
+  measurementId: "G-GCHMMKGWWW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
